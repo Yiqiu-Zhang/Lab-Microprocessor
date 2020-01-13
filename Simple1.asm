efed43b12b93ccaf02d70c4596eeee41233f7a55
yz13617 <yz13617@skbl-422-007.ph.ic.ac.uk>
2020-01-13 15:54:58 +0000

    DELAY COUNTER

--- Simple1.asm
+++ Simple1.asm
@@ -6,16 +6,26 @@
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-start
-	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
+start	
+	movlw 	0xff
+	movwf	TRISD, ACCESS	    ; Port D all INputs
+	movlw	0x00
+	movwf	TRISC, ACCESS	    ; Port C all outputs
 	bra 	test
-loop	movff 	0x06, PORTB
+loop	movff 	0x06, PORTC
+	movlw	0xff
+	movwf	0x20,	ACCESS	    ;Address which stores the cycle that delayed	
+	call	delay
 	incf 	0x06, W, ACCESS
+	
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
+	movf	PORTD, W, ACCESS    ; Assign VALUE OF PORTD TO W
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
+
+delay	decfsz	0x20, F, ACCESS
+	bra delay
+	return
 	
 	end
