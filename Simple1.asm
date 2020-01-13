b3a0cf84370179ed954b4669c4c1c61967e4ad98
yz13617 <yz13617@skbl-422-007.ph.ic.ac.uk>
2020-01-13 17:07:49 +0000

    store data

--- Simple1.asm
+++ Simple1.asm
@@ -11,22 +11,16 @@ setup	bcf	EECON1, CFGS	; point to Flash program memory
 	bsf	EECON1, EEPGD 	; access Flash program memory
 	goto	start
 	; ******* My data and where to put it in RAM *
-myTable data	"This is just some data"
-	constant 	myArray=0x400	; Address in RAM for data
-	constant 	counter=0x10	; Address of counter variable
-	; ******* Main programme *********************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	.22		; 22 bytes to read
-	movwf 	counter		; our counter register
-loop 	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0	; move read data from TABLAT to (FSR0), increment FSR0	
-	decfsz	counter		; count down to zero
+	
+start	lfsr	FSR0, 0x400	; Load FSR0 with address in RAM	
+	movlw	0xff		; 22 bytes to read
+	movwf 	0x10		; our counter register
+	movlw	0x00
+	movwf	0x50
+
+loop	movff	0x50, POSTINC0
+	incf	0x50, f
+	decfsz	0x10		; count down to zero
 	bra	loop		; keep going until finished
 	
 	goto	0
