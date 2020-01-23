a168d955d2224ae62dd870a8b883640678701890
yz13617 <yz13617@skbl-422-007.ph.ic.ac.uk>
2020-01-23 14:16:02 +0000

    SPI

--- Simple1.asm
+++ Simple1.asm
@@ -2,42 +2,51 @@
 	
 	code
 	org 0x0
-	goto	setup
+	call	SPI_MasterInit
+	goto	SPI_MasterTransmit
 	
-	org 0x100		    ; Main code starts here at address 0x100
-
-	; ******* Programme FLASH read Setup Code ****  
-setup	bcf	EECON1, CFGS	; point to Flash program memory  
-	bsf	EECON1, EEPGD 	; access Flash program memory
-	constant myarray = 0x400 
-	constant counter = 0x10
-	constant number = 0x50
-	constant myread = 0x500
-	;constant
-	goto	start
+	org 0x100		    ; Main code starts here at address 0x100	
 	; ******* My data and where to put it in RAM *
 	
-start	lfsr	FSR0, myarray	; Load FSR0 with address in RAM	
-	lfsr	FSR1, myread
-	movlw	0x00
-	movwf	TRISC, ACCESS
-	movlw	0xf1		; 22 bytes to read
-	movwf 	counter		; our counter register
-	movlw	0x00
-	movwf	number
+SPI_MasterTransmit  ; Start transmission of data (held in W)
+	movlw	0x53
+	movwf 	SSP2BUF 
+	
+	call Wait_Transmit
 
-loop	movff	number, INDF0
-	movff	INDF0, INDF1
-	movf	POSTINC0, w
-	cpfseq	POSTINC1, ACCESS
-	call	wrong
-	incf	number, f
-	decfsz	counter		; count down to zero
-	bra	loop		; keep going until finished
+	movlw	0xff
+	movwf	mydelay
+	call delay
 	
 	goto	0
-wrong	movff	0x06, PORTC
-	movlw	0x05
-	movwf	0x06, ACCESS
+	
+Wait_Transmit	; Wait for transmission to complete 
+	btfss 	PIR2, SSP2IF
+	bra 	Wait_Transmit
+	bcf 	PIR2, SSP2IF	; clear interrupt flag
+	return		
+	
+SPI_MasterInit	; Set Clock edge to negative
+	constant    mydelay = 0x50
+	constant    mydelay1 = 0x60
+	bcf	SSP2STAT, CKE
+	; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+	movlw 	(1<<SSPEN)|(1<<CKP)|(0x02)
+	movwf 	SSP2CON1
+	; SDO2 output; SCK2 output
+	bcf	TRISD, SDO2
+	bcf	TRISD, SCK2
+	return 
+
+delay	decfsz	mydelay, F, ACCESS
+	movlw	0xff
+	movwf	mydelay1
+	call	delay1		; anather dealy loop
+	bra	delay
 	return
+
+delay1	decfsz	mydelay1, F, ACCESS
+	bra	delay1
+	return
+
 	end
